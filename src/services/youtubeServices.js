8c2867a92ea3e9fc614a76b9d8f040526f7e3ceb
Onyiriuka Justin <richardjustin500@gmail.com>
2024-10-12 15:18:20 +0100

    :

--- src/services/youtubeServices.js
+++ src/services/youtubeServices.js
@@ -1,19 +1,15 @@
 import ytdl from "@distube/ytdl-core";
 import { errorResponse } from "../utils/responseHandlers.js";
+import fs from "fs";
 
 export default async function getVideo(videoURL, format, resolution, res) {
   console.log(`URL: ${videoURL}, Format: ${format}, Resolution: ${resolution}`);
   try {
-    const info = await ytdl.getInfo(videoURL, {
-      requestOptions: {
-        headers: {
-          "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-          Cookie:
-            "SIDCC=AKEyXzVq5RLYH1RAbx6dmNTkUIMAjS91O_XbF1avAcMv8QaGAujz66e476eBjK_c6WPeTWJy; __Secure-1PSIDCC=AKEyXzWUWDdCdGAQZMW7YF0Fko1THfWNlAd0te0SQdB3o5CgJXJaqcMEDxkYFgKcK3xeHsOPuFQ; __Secure-3PSIDCC=AKEyXzVhTvV8eKhHUhaPBudb_TIT-dr7tC07vnVPzrZb6H9jyei4ihMY8QesUFlhpOQ3mb6sJ",
-        },
-      },
-    });
+    const agent = ytdl.createAgent(
+      JSON.parse(fs.readFileSync("cooks.json"))
+    );
+
+    const info = await ytdl.getInfo(videoURL, { agent });
     console.log("Got video info:", info);
 
     const selectedFormat = ytdl.chooseFormat(info.formats, {
@@ -39,16 +35,7 @@ export default async function getVideo(videoURL, format, resolution, res) {
 
     console.log("Starting to stream video...");
 
-    ytdl(videoURL, {
-      format: selectedFormat,
-      requestOptions: {
-        headers: {
-          "User-Agent": "Mozilla/5.0",
-          Cookie:
-            "SIDCC=AKEyXzVq5RLYH1RAbx6dmNTkUIMAjS91O_XbF1avAcMv8QaGAujz66e476eBjK_c6WPeTWJy; __Secure-1PSIDCC=AKEyXzWUWDdCdGAQZMW7YF0Fko1THfWNlAd0te0SQdB3o5CgJXJaqcMEDxkYFgKcK3xeHsOPuFQ; __Secure-3PSIDCC=AKEyXzVhTvV8eKhHUhaPBudb_TIT-dr7tC07vnVPzrZb6H9jyei4ihMY8QesUFlhpOQ3mb6sJ",
-        },
-      },
-    })
+    ytdl(videoURL, { format: selectedFormat, agent })
       .pipe(res)
       .on("error", (err) => {
         console.log("Error streaming video:", err);
