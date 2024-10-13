4597a0d363bc4691519deb441a4ac6fc8a3dc92e
Onyiriuka Justin <richardjustin500@gmail.com>
2024-10-13 22:59:00 +0100

    updates

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 const limiter = rateLimit({
   windowMs: 20 * 60 * 1000, // 20 minutes
-  max: 10, // limit each IP to 10 requests per windowMs
+  max: 100, // limit each IP to 10 requests per windowMs
   message: "Too many requests from this IP, please try again after 15 minutes",
 });
 
